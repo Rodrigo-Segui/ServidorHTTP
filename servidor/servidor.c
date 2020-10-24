de65263c8922d1c7a3f459c93324fbe8bdd259ea
Rodrigo-Segui <rodrigo_segui@hotmail.com>
2020-10-24 20:45:12 -0300

    modifcacoes video

--- servidor/servidor.c
+++ servidor/servidor.c
@@ -3,32 +3,39 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 #define PORT 8080
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    int socket_servidor, socket_cliente, valread;
     int opt = 1;
-    int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
+    
+    struct sockaddr_in address = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = INADDR_ANY,
+        .sin_port = htons( PORT )
+    };
+
+    int addrlen = sizeof(address);
+
+
      
     // criando o socket - funcao socket
     //int socket(int domain, int type, int protocol)
     //AF_INET protocolo para comunicaçao ipv4
     //tipo de comunicacao - sock_stream - comunicacao de dois lados baseada em conexao
     // 0 procolo tcp
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    socket_servidor = socket(AF_INET, SOCK_STREAM, 0);
+    if ((socket_servidor) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
      
     // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+    if (setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
         perror("setsockopt");
@@ -42,13 +49,16 @@ int main(int argc, char const *argv[])
     //sockfd -socket que queremos associar um endereco a ele
     // ponteiro para uma estrutura socketaddr- info familia de protocolos, endereco na rede local  e porta
     // tamanho em bytes das estruturas passadas no segundo argumento
-    if (bind(server_fd, (struct sockaddr *)&address,
+    if (bind(socket_servidor, (struct sockaddr *)&address,
                                  sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
+
+    // permitir que o socket receba conexoes
+    //maximo de conexoes simulaneas
+    if (listen(socket_servidor, 5) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
@@ -57,18 +67,21 @@ int main(int argc, char const *argv[])
     while(1){
 
         printf("\n-----Aguardando por uma nova conexão-----\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                        (socklen_t*)&addrlen))<0)
+        //funcao accept  espera por conecoes
+        socket_cliente = accept(socket_servidor, (struct sockaddr *)&address,
+                        (socklen_t*)&addrlen);
+        if (socket_cliente < 0)
         {
             perror("accept");
             exit(EXIT_FAILURE);
         }
 
-        valread = read( new_socket , buffer, 1024);
+        valread = read( socket_cliente , buffer, 1024);
+
         printf("%s\n",buffer );
-        send(new_socket , hello , strlen(hello) , 0 );
+        send(socket_cliente , hello , strlen(hello) , 0 );
         printf("Hello message sent\n");
-        close(new_socket);
+        close(socket_cliente);
     }   
     return 0;
 }
