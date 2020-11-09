fe87b803b844e1134a9e01310e8eb34987d1ac60
Rodrigo-Segui <rodrigo_segui@hotmail.com>
2020-11-09 00:00:59 -0300

    pastas organizadas

---
+++ version1/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <pthread.h>   // for threading, link with lpthread
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include "server_function.h"
+
+//sem_t mutex; // para controlar o contador de threads
+
+int main()
+{
+
+    printf("----- Servidor HTTP ------\n\n");
+    //sem_init(&mutex, 0, 1); // Inıcializa mutex com 1.
+    // Variaveis socket servidor, vetor de socket de clientes
+    int socket_server, socket_client, *new_socket_client;
+    int opt=1;
+
+//    sem_init(&mutex,0,1); // inicializa mutex com 1
+
+    // Declaraçao de porta padrao, taxa maxima, tamanho cliente
+    int PORT = 8080;
+    int MAX_RATE = 1000;
+    int LENGTH_CLIENT;
+
+    struct sockaddr_in client;
+    LENGTH_CLIENT = sizeof(client);
+
+
+
+    // Informaçoes do servidor
+    printf("Mensagem do Servidor | Status      : Online!\n");
+    printf("Mensagem do Servidor | Taxa Maxima : %d \n", MAX_RATE);
+    printf("Mensagem do Servidor | Porta       : %d \n", PORT);
+
+    // criação do socket
+    //socket(int domain, int type, int protocol)
+    // PARAMETROS
+    // 1 - AF_INET protocolo para comunicaçao ipv4
+    // 2 - tipo de comunicacao - sock_stream - comunicacao de dois lados baseada em conexao
+    // 3 - protocolo tcp
+    socket_server = socket(AF_INET, SOCK_STREAM, 0);
+
+    // verificacao de erro socket
+    if (socket_server < 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(socket_server, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    bzero(&client, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+    client.sin_addr.s_addr = INADDR_ANY;
+
+    // Associa o socket a um endereco na rede/local - funcao bind
+    // bind(int sockfd, const struct sockaddr *addr, soclen_t addrlen)
+    // ARGUMENTOS
+    // 1 - sockfd -socket que queremos associar um endereco a ele
+    // 2 - ponteiro para uma estrutura socketaddr- info familia de protocolos, endereco na rede local  e porta
+    // 3 -tamanho em bytes das estruturas passadas no segundo argumento
+
+    if (bind(socket_server, (struct sockaddr *)&client, LENGTH_CLIENT) == -1)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //permitir que o socket receba conexoes
+    //maximo de conexoes simulaneas
+    if (listen(socket_server, MAX_CONNECTIONS) == -1)
+    {
+       perror("ERRO listen");
+       exit(EXIT_FAILURE);
+    }
+
+    int clients_number = 0;
+
+    while (1)
+    {
+        socket_client = accept(socket_server, (struct sockaddr *)&client, &LENGTH_CLIENT);
+        printf("Recebeu conexão cliente: %i\n", clients_number);
+
+        pthread_t sniffer_thread; // nova thread
+        new_socket_client = (int*) malloc(1);
+        *new_socket_client = socket_client;
+
+        //connectionandtreatMessage((void *)new_socket_client);
+
+        if (pthread_create(&sniffer_thread, NULL, connectionandtreatMessage, (void *)new_socket_client) < 0){ // cria uma thread para cada requisicao, passando socket novo
+        puts("Could not create thread");
+        return 1;
+      }
+
+    }
+
+    close(socket_server);
+    return 0;
+}
---
+++ version1/server_function.h
@@ -0,0 +1,13 @@
+
+#define MAX_CONNECTIONS 3
+#define LENGTH_MESSAGE 1025
+#define USER_RATE 1
+
+
+
+//DECLARACAO DA FUNCOES
+void sendFile(char *file_path, FILE *file_pointer, int socket, int rate);
+char *treatFileType(char *type, struct stat fileinfo);
+void treatFile(char *file_path, FILE *file_point, int socket);
+void *connectionandtreatMessage( void *new_sock);
+
