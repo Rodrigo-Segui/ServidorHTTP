ca833d725d7411bb5fb854bc0d84024d11fe1d68
arthurteixeira <arthurtjardim@gmail.com>
2020-11-27 17:29:54 -0300

    Versão 3.1

---
+++ version3.1/server_function.h
@@ -0,0 +1,17 @@
+
+#define MAX_CONNECTIONS 10
+#define LENGTH_MESSAGE 125
+#define USER_RATE 1
+
+sem_t mutex; // para controlar o contador de threads
+sem_t mutex_rate;
+struct sockaddr_in server, client;
+#define PATH "/home/arthur/new/ServidorHTTP/version3/arquivos/"
+
+//DECLARACAO DA FUNCOES
+int rateControl();
+void sendFile(char *file_name, int socket, int rate, char *type);
+void treatFileType(char *file_path, void *new_sock);
+void treatFile(char *message, void *new_sock);
+void *treatMessage( void *new_sock);
+
