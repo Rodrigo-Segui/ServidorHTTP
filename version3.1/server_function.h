eec23f0e30763bb3bbba6d70ccec9b99670afb7b
arthurteixeira <arthurtjardim@gmail.com>
2020-11-27 23:52:11 -0300

    Upanda versão com erro

--- version3.1/server_function.h
+++ version3.1/server_function.h
@@ -6,7 +6,7 @@
 sem_t mutex; // para controlar o contador de threads
 sem_t mutex_rate;
 struct sockaddr_in server, client;
-#define PATH "/home/arthur/new/ServidorHTTP/version3/arquivos/"
+#define PATH "/home/arthur/redes/ServidorHTTP/version3/arquivos/"
 
 //DECLARACAO DA FUNCOES
 int rateControl();
